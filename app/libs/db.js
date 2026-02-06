b027fa24ad3523bccca634e7cdc18f63aa88cb01
mukhlisafridi <mukhlisafridi4@gmail.com>
2026-02-06 19:10:33 +0500

    test

--- app/libs/db.js
+++ app/libs/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const DBConnection=async()=>{
     try {
-       await mongoose.connect("mongodb+srv://mukhlisafridi4_db_user:afridi@cluster0.s4cpju6.mongodb.net/Nextapp")
+       await mongoose.connect("etc")
         console.log('mongodb connected')
     } catch (error) {
         console.log('mongodb error',error)
